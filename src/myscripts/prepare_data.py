d2b170b53324c4af51a228ddf03ae04a230ca463
Bartosz Piłat <01187576@pw.edu.pl>
2025-04-19 21:43:45 +0200

    Added model_testing notebook

--- src/myscripts/prepare_data.py
+++ src/myscripts/prepare_data.py
@@ -20,7 +20,7 @@ def _encode_labels(df, label_col_index, encoder_class):
     dataframe.iloc[:, label_col_index] = Y
     return dataframe
 
-def prepare_data_for_model(df, label_col_index, encoder_class, split_proportions):
+def prepare_data_for_model(df, label_col_index, encoder_class, split_proportions, random_state=0, save_to_npy=True):
     path= config.DATA_SPLIT_SAVE_DIR_PATH
     if(sum(split_proportions)!=1):
         raise AttributeError("split_proportions does not add up to 1. (correct example: split_proportions=[0.7,0.2,0.1])")
@@ -29,25 +29,28 @@ def prepare_data_for_model(df, label_col_index, encoder_class, split_proportions
     Y = data_encoded.iloc[:, -1].values
 
     # Podzial zbioru na dane do uczenia, zbior validacyjny oraz zbior do pozniejszego testowania danych.
-    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=1-split_proportions[0], random_state=0, shuffle=True, stratify=Y)
-    x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size=split_proportions[2]/(split_proportions[1]+split_proportions[2]), random_state=0, shuffle=True,
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=1-split_proportions[0], random_state=random_state, shuffle=True, stratify=Y)
+    x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size=split_proportions[2]/(split_proportions[1]+split_proportions[2]), random_state=random_state, shuffle=True,
                                                     stratify=y_test)
 
     # Rozszerzenie wymiarów danych, ponieważ dla CNN potrzeba 3 wymiarow (wysokosc, szerokosc i ilosc kanalow obrazu)
     x_train = np.expand_dims(x_train, axis=-1)
     x_test = np.expand_dims(x_test, axis=-1)
     x_val = np.expand_dims(x_val, axis=-1)
-    np.save(os.path.join(path, "x_train.npy"), x_train)
-    np.save(os.path.join(path, "y_train.npy"), y_train)
-    np.save(os.path.join(path, "x_val.npy"), x_val)
-    np.save(os.path.join(path, "y_val.npy"), y_val)
-    np.save(os.path.join(path, "x_test.npy"), x_test)
-    np.save(os.path.join(path, "y_test.npy"), y_test)
-    print(f"Prepared data saved in: {path}.\nData shape confirmation:"
-          f"\nx_train:{x_train.shape}"
-          f"\ny_train:{y_train.shape}"
-          f"\nx_val:{x_val.shape}"
-          f"\ny_val:{y_val.shape}"
-          f"\nx_test:{x_test.shape}"
-          f"\ny_test:{y_test.shape}"
-          f"\n\nNumber of classes: {len(np.unique(y_train))}")
+    if save_to_npy:
+        np.save(os.path.join(path, "x_train.npy"), x_train)
+        np.save(os.path.join(path, "y_train.npy"), y_train)
+        np.save(os.path.join(path, "x_val.npy"), x_val)
+        np.save(os.path.join(path, "y_val.npy"), y_val)
+        np.save(os.path.join(path, "x_test.npy"), x_test)
+        np.save(os.path.join(path, "y_test.npy"), y_test)
+        print(f"Prepared data saved in: {path}.\nData shape confirmation:"
+              f"\nx_train:{x_train.shape}"
+              f"\ny_train:{y_train.shape}"
+              f"\nx_val:{x_val.shape}"
+              f"\ny_val:{y_val.shape}"
+              f"\nx_test:{x_test.shape}"
+              f"\ny_test:{y_test.shape}"
+              f"\n\nNumber of classes: {len(np.unique(y_train))}")
+    else:
+        return x_train, y_train, x_val, y_val, x_test, y_test
